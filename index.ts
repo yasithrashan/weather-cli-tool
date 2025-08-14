4d61333e4b025f144887960c3311c4e0fde8d9ad
Yasith Rashan <y.rashan22@gmail.com>
2025-08-14 11:13:43 +0530

    feat: implement openweather api for real weather data

--- index.ts
+++ index.ts
@@ -1,51 +1,49 @@
-import { z } from "zod";
+import { number, z } from "zod";
 import { generateText, tool } from "ai";
 import { google } from "@ai-sdk/google";
 
+
+const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
+
 const weatherTool = tool({
   name: "weather",
-  description: "Get the weather in a location",
+  description: "Get the real weather in a location using OpenWeather API",
   inputSchema: z.object({
     location: z.string().describe("The location to get the weather for"),
   }),
-
   execute: async ({ location }) => {
-    const temperature = 72 + Math.floor(Math.random() * 21) - 10;
-    return {
-      location,
-      temperature,
+    const geo = await getGeoLocation(location);
+    if (geo) {
+      await getWeather(geo.lat, geo.lon)
     }
-  },
+  }
 });
 
-async function main() {
-  try {
-    const args = process.argv.slice(2);
-    if (args.length === 0) {
-      console.log("usage: bun run index.ts <location>");
-      return;
-    }
-    const location = args.join(" ");
+async function getGeoLocation(location: string): Promise<{ name: string, lat: number, lon: number } | undefined> {
+  const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(
+    location
+  )}&limit=1&appid=${OPENWEATHER_API_KEY}`;
 
-    console.log(`Getting weather for: ${location}`);
+  const geoRes = await fetch(geoUrl);
 
-    const result = await generateText({
-      model: google("gemini-1.5-flash-latest"),
-      tools: { weather: weatherTool },
-      prompt: `Use the weather tool to tell me the weather in ${location}.`,
-    });
+  if (!geoRes.ok) throw new Error(`Geocoding API error: ${geoRes.statusText}`);
+  const result = (await geoRes.json()) as { name: string, lat: number, lon: number }[];
 
-    if (result.toolResults.length > 0) {
-      for (const toolResult of result.toolResults) {
-        console.log(`Tool "${toolResult.toolName}" returned:`, toolResult.output);
-      }
-    } else {
-      console.log("Text:", result.text);
-    }
-  } catch (error) {
-    console.error("Error:", error);
+  if (!result.length) {
+    throw new Error(`No location found for "${location}"`);
   }
+  return result[0];
+}
+
+async function getWeather(lat: number, lon: number) {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPENWEATHER_API_KEY}`;
+
+  const weatherRes = await fetch(weatherUrl);
+  if (!weatherRes.ok) throw new Error(`Weather API error: ${weatherRes.statusText}`);
+
+  const data = await weatherRes.json();
+  return data;
 }
 
 
-main();
+
